d12b087d25c07fc479b5c02fff57f75862c5da38
swaroopa <swaroopa@.com>
2024-04-09 15:40:19 +0530

    first

--- project/public/menuToggle.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('#menu-btn');
-const menu = document.querySelector('#menu');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-  menu.classList.toggle('block');
-});
